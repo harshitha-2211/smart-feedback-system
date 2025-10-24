f30d3101005c893169aa11298d919762e4afe988
Harshitha Guntoju <harshithaguntoju@gmail.com>
2025-10-25 03:24:50 +0530

    Intial commit

--- backend/server.js
+++ backend/server.js
@@ -37,5 +37,5 @@ app.get(["/", "/index.html"], (req, res) => {
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
