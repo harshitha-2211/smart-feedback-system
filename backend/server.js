9f03521f7ebe386356957f0511267d25b9fbdf1a
Harshitha Guntoju <harshithaguntoju@gmail.com>
2025-10-25 10:50:30 +0530

    Intial commit

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.get("/", (req, res) => {
   res.send("Smart Feedback System backend is running!");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT||5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
